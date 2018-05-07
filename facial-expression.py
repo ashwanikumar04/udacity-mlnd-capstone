f4b067b9f950f5b2d1763b808d296903345577a0
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-05-07 11:06:00 +0530

    updated

--- facial-expression.py
+++ facial-expression.py
@@ -6,6 +6,7 @@ import random
 from sklearn.utils import shuffle
 import tensorflow as tf
 from helpers import one_hot_encode, get_batch, get_training_set, get_test_set, log
+import matplotlib.pyplot as plt
 from params import HyperParameter
 from linear_classifier import LinearClassifer
 from cnn import CNN
@@ -19,14 +20,23 @@ validate_X, validate_y = get_test_set(df.loc[df['Usage'] == 'PrivateTest'])
 labels = 7
 image_size = 2304
 
-params = HyperParameter(num_batches=1, batch_size=1,
-                        epoch=3, learning_rate=.001, hold_prob=0.5, epoch_to_report=1)
+params = HyperParameter(num_batches=64, batch_size=32,
+                        epoch=500, learning_rate=.001, hold_prob=0.5, epoch_to_report=1)
 
 log(params)
 
-#model = LinearClassifer(params=params, labels=labels, image_size=image_size)
-model = CNN(params=params, labels=labels, image_size=image_size)
+model = LinearClassifer(params=params, labels=labels, image_size=image_size)
+#model = CNN(params=params, labels=labels, image_size=image_size)
+
+accuracyDictionary, loss_trace, train_acc, test_acc = model.run(train_X=train_X, train_y=train_y, test_X=test_X,
+                                                                test_y=test_y, validate_X=validate_X, validate_y=validate_y)
+
+# log(model.run(train_X=train_X, train_y=train_y, test_X=test_X,
+#               test_y=test_y, validate_X=validate_X, validate_y=validate_y))
 
 
-log(model.run(train_X=train_X, train_y=train_y, test_X=test_X,
-              test_y=test_y, validate_X=validate_X, validate_y=validate_y))
+plt.plot(loss_trace)
+plt.title('Cross Entropy Loss')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
--- linear_classifier.py
+++ linear_classifier.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from helpers import one_hot_encode, get_batch, get_training_set, get_test_set, log
 from sklearn.utils import shuffle
 
+
 class LinearClassifer:
     def __init__(self, params, labels, image_size):
         self.params = params
@@ -9,20 +10,23 @@ class LinearClassifer:
         self.image_size = image_size
 
     def run(self, train_X, train_y, test_X, test_y, validate_X, validate_y):
-        accuracyDictionary = {};
+        accuracyDictionary = {}
         x = tf.placeholder(tf.float32, shape=[None, self.image_size])
         W = tf.Variable(tf.zeros([self.image_size, self.labels]))
         b = tf.Variable(tf.zeros([self.labels]))
         y = tf.matmul(x, W) + b
         y_true = tf.placeholder(tf.float32, [None, self.labels])
-        cross_entropy = tf.reduce_mean(
+        loss = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=y))
         optimizer = tf.train.GradientDescentOptimizer(
             learning_rate=self.params.learning_rate)
-        train = optimizer.minimize(cross_entropy)
+        goal = optimizer.minimize(loss)
         init = tf.global_variables_initializer()
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_true, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        loss_trace = []
+        train_acc = []
+        test_acc = []
         with tf.Session() as sess:
             sess.run(init)
             for step in range(self.params.epoch):
@@ -30,13 +34,24 @@ class LinearClassifer:
                 for current_batch in range(self.params.num_batches):
                     batch_X, batch_y = get_batch(
                         X, y, current_batch, self.params.batch_size)
-                    sess.run(train, feed_dict={x: batch_X, y_true: batch_y})
+                    sess.run(goal, feed_dict={x: batch_X, y_true: batch_y})
                 if step % self.params.epoch_to_report == 0:
-                    log(step,"Epoch")
-                    accuracyDictionary[step]=sess.run(accuracy, feed_dict={x: test_X,
-                                                      y_true: test_y})
+                    log(step, "Epoch")
+                    temp_loss = sess.run(
+                        loss, feed_dict={x: batch_X, y_true: batch_y})
+                    # convert into a matrix, and the shape of the placeholder to correspond
+                    temp_train_acc = sess.run(
+                        accuracy, feed_dict={x: train_X, y_true: train_y})
+                    temp_test_acc = sess.run(accuracy, feed_dict={
+                                             x: test_X, y_true: test_y})
+                    # recode the result
+                    loss_trace.append(temp_loss)
+                    train_acc.append(temp_train_acc)
+                    test_acc.append(temp_test_acc)
+                    accuracyDictionary[step] = sess.run(accuracy, feed_dict={x: test_X,
+                                                                             y_true: test_y})
                     log(accuracyDictionary[step], "model accuracy")
 
             log(sess.run(accuracy, feed_dict={x: validate_X,
                                               y_true: validate_y}), "Final accuracy")
-        return accuracyDictionary
+        return accuracyDictionary, loss_trace, train_acc, test_acc
